e399c72e6cda78fc1e0c7efb7b595bcc608d46f1
RJR <80534043+rjr2@users.noreply.github.com>
2021-08-16 15:33:37 -0400

    update README

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# notetaker
+# NoteTaker
+## Description
+<p> A small note taking application that will create, save, delete, and display notes created in a web browser. </p>
+
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require('path');
-const notes = require('./routes/notes');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
